66e06d703841fae9adb35fa65184fbe62179f231
DevElo <147736887+Dev-Elo-1505@users.noreply.github.com>
2026-04-07 13:22:53 +0100

    feat(notifications): implement notifications feed

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,11 +1,41 @@
-import { View, Text } from 'react-native'
+import { Loader } from "@/components/Loader";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { styles } from "@/styles/notifications.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { useQuery } from "convex/react";
+import { FlatList, Text, View } from "react-native";
+import Notification from '@/components/Notification'
 
 const Notifications = () => {
-    return (
-        <View>
-            <Text>Notif screen yooo!</Text>
-        </View>
-    )
-}
+  const notifications = useQuery(api.notifications.getNotifications);
 
-export default Notifications
+  if (notifications === undefined) return <Loader />;
+  if (notifications.length === 0) return <NoNotificationsFound />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Notifications</Text>
+      </View>
+
+      <FlatList data={notifications} renderItem={({ item }) => <Notification notification={item} />}
+        keyExtractor={(item) => item._id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.listContainer} />
+    </View>
+  );
+};
+
+export default Notifications;
+
+function NoNotificationsFound() {
+  return (
+    <View style={[styles.container, styles.centered]}>
+      <Ionicons name="notifications-outline" size={48} color={COLORS.primary} />
+      <Text style={{ fontSize: 20, color: COLORS.white }}>
+        No notifications yet
+      </Text>
+    </View>
+  );
+}
